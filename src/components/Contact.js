ce38fc0d615cc1a963f1b2c822e03a55b16d79d6
PatilAneesh <91588568+PatilAneesh@users.noreply.github.com>
2023-04-13 12:21:23 +0530

    2 files modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import './Contact.css';
 const Contact = () => {
   return (
     <>
-      <div>Contact</div>
+      <h1>Contact</h1>
+      <div className="card cards">
+        <h4>Name: Aneesh Patil</h4>
+        <h4>Phone.No: +91 7022870534</h4>
+        <h4>Email: aneeshpatil341@gmail.com</h4>
+        <h4>
+          LinkedIn:{' '}
+          <a
+            href="https://www.linkedin.com/in/aneesh-patil-65b54a183/"
+            target="_blank"
+          >
+            Click Here
+          </a>
+        </h4>
+        <h4>
+          GitHub:{' '}
+          <a href="https://github.com/PatilAneesh" target="_blank">
+            Click Here
+          </a>
+        </h4>
+      </div>
     </>
   );
 };
